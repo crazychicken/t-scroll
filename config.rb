afbb1e9e21e390cc275f427a18286a27106f4679
tuds <crazychicken@clgt.vn>
2017-07-06 18:22:12 +0700

    update name file sass

---
+++ config.rb
@@ -0,0 +1,27 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = '/'
+sass_path = './sass'
+css_dir = './public/theme/css'
+
+
+# You can select your preferred output style
+# here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
+
+sourcemap = false
+
+line_comments = true
+
+# To enable relative paths to assets via
+# compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the
+# original location of your selectors. Uncomment:
+# line_comments = false
+
+# Comment this out to default to SCSS:
+preferred_syntax = :sass
